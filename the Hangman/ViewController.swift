175379d3f8726e168b984a82372b217650c7795e
Michele Galvagno <michicello@gmail.com>
2019-03-15 11:33:41 +0100

    Initial Commit

---
+++ the Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  the Hangman
+//
+//  Created by Michele Galvagno on 15/03/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
